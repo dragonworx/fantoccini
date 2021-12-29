138cd5f1865eae866d3b48f3d2968a79b3323dd2
dragonworx <dragonworxau@yahoo.com.au>
2021-12-29 21:07:16 +1100

    Update control.ts

--- src/ocd-kit/core/control.ts
+++ src/ocd-kit/core/control.ts
@@ -1,6 +1,7 @@
 import EventEmitter from 'eventemitter3';
 import { DynamicStyleSheet, CSSRuleNode, css, CSSRuleKey } from './stylesheet';
 import { Element } from './element';
+import { Text } from '../components/text';
 
 export { css };
 
@@ -8,15 +9,18 @@ export function px(value: string | number) {
   return typeof value === 'string' ? value : `${value}px`;
 }
 
+const fn = <T extends unknown, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key];
+};
+
 let id: number = 0;
 
 export interface BaseProps {
-  tag: string;
+  tag?: string;
   visible: boolean;
 }
 
 export const defaultProps: BaseProps = {
-  tag: 'a',
   visible: true,
 };
 
@@ -30,8 +34,8 @@ export type V<Props> = Props[keyof Props];
 /**
  * BaseControl
  */
-export abstract class BaseControl<
-  Props extends Record<string, any>,
+export abstract class Control<
+  Props extends BaseProps,
   RootElement extends HTMLElement
 > extends EventEmitter {
   protected readonly _id: number;
@@ -39,8 +43,8 @@ export abstract class BaseControl<
   protected readonly element: RootElement;
   protected readonly elementRef: Element;
   protected readonly styleSheet: DynamicStyleSheet;
-  protected readonly children: BaseControl<any, any>[];
-  protected parent?: BaseControl<any, any>;
+  protected readonly children: Control<any, any>[];
+  protected parent?: Control<any, any>;
   isMounted: boolean;
 
   private static parseHTML<T>(html: string): T {
@@ -59,7 +63,7 @@ export abstract class BaseControl<
     } as unknown as Props;
     this.props = propsWithDefaults;
     const { html, style } = this.template();
-    this.element = BaseControl.parseHTML<RootElement>(html);
+    this.element = Control.parseHTML<RootElement>(html);
     this.element.setAttribute('data-id', this.id);
     if (propsWithDefaults.tag) {
       this.element.setAttribute('data-tag', propsWithDefaults.tag);
@@ -81,18 +85,18 @@ export abstract class BaseControl<
       )
     );
 
-    Object.keys(propsWithDefaults).forEach((key) => {
-      Object.defineProperty(this, key, {
-        get: () => this.props[key],
-        set: (value: V<Props>) => {
-          (this.props as Record<string, any>)[key] = value;
-          const props: Partial<Props> = {};
-          props[key as K<Props>] = value;
-          this.updateProps(props);
-          return this;
-        },
-      });
-    });
+    // Object.keys(propsWithDefaults).forEach((key) => {
+    //   Object.defineProperty(this, key, {
+    //     get: () => this.props[key],
+    //     set: (value: V<Props>) => {
+    //       (this.props as Record<string, any>)[key] = value;
+    //       const props: Partial<Props> = {};
+    //       props[key as K<Props>] = value;
+    //       this.updateProps(props);
+    //       return this;
+    //     },
+    //   });
+    // });
 
     this.bindDomEvents();
     this.init();
@@ -105,12 +109,13 @@ export abstract class BaseControl<
     if ('visible' in props) {
       styleSheet.root.set('display', props.visible ? '' : 'none');
     }
-    Object.keys(props).forEach((key) => {
-      const value = props[key] as V<Props>;
-      if (styleSheet.isCssProperty(key)) {
+    Object.keys(props).forEach((k) => {
+      const key = k as K<Props>;
+      const value = props[key as K<Props>] as V<Props>;
+      if (styleSheet.isCssProperty(k)) {
         const cssValue = this.convertPropToCssValue(key, value);
         const selector = this.cssSelectorForPropUpdate(key);
-        this.css(selector).set(key as CSSRuleKey, cssValue);
+        this.css(selector).set(key as CSSRuleKey, String(cssValue));
       }
       this.onUpdate(key, value);
       this.emit('update', props);
@@ -163,7 +168,19 @@ export abstract class BaseControl<
   protected init() {}
 
   get id() {
-    return `arena-ctrl-${this._id}`;
+    return `ocdkit-ctrl-${this._id}`;
+  }
+
+  get<T extends Props, K extends keyof T>(key: K): T[K] {
+    return (this.props as any)[key];
+  }
+
+  set(propKey: K<Props>, value: V<Props>) {
+    this.props[propKey] = value;
+    const props: Partial<Props> = {};
+    props[propKey] = value;
+    this.updateProps(props);
+    return this;
   }
 
   mount(containerElement: HTMLElement | null) {
@@ -222,7 +239,7 @@ export abstract class BaseControl<
     return new Element(element);
   }
 
-  add(control: BaseControl<any, any>, refName?: string) {
+  add(control: Control<any, any>, refName?: string) {
     let element: HTMLElement = this.element;
     if (refName) {
       element = this.ref(refName);
@@ -233,7 +250,7 @@ export abstract class BaseControl<
     return this;
   }
 
-  remove(control: BaseControl<any, any>) {
+  remove(control: Control<any, any>) {
     delete control.parent;
     const index = this.children.indexOf(control);
     this.children.splice(index, 1);
@@ -242,8 +259,123 @@ export abstract class BaseControl<
   }
 }
 
-export const Control = BaseControl as unknown as {
-  new <Props extends Record<string, any>, RootElement extends HTMLElement>(
-    props: Props
-  ): BaseControl<Props, RootElement> & Props & BaseProps;
+// export const Control = BaseControl as unknown as {
+//   new <Props extends Record<string, any>, RootElement extends HTMLElement>(
+//     props: Props
+//   ): BaseControl<Props, RootElement> & Props & BaseProps;
+// };
+
+// export function Wrapper<Props, Class>(
+//   cls: unknown
+// ): {
+//   new (props: Props): Class & Props;
+// } {
+//   return cls as unknown as {
+//     new (props: Props): Class & Props;
+//   };
+// }
+
+export function WithBase<Props, Element extends HTMLElement, SubClass = {}>(
+  cls: unknown = Base
+): {
+  new (props: Props): Base<Props, Element> & Omit<SubClass, 'element'> & Props;
+} {
+  return cls as {
+    new (props: Props): Base<Props, Element> &
+      Omit<SubClass, 'element'> &
+      Props;
+  };
+}
+
+export type PropsBase = {
+  id: string;
 };
+
+export type SubProps = PropsBase & {
+  subA: string;
+  subB?: number;
+};
+
+export type SubProps1 = SubProps & {
+  subC: boolean;
+};
+
+export class Base<Props extends Record<string, any>, E extends HTMLElement> {
+  element: E;
+  props: Props;
+
+  constructor(props: Props) {
+    this.element = {} as any;
+    this.props = props;
+    Object.keys(props).forEach((key) => {
+      Object.defineProperty(this, key, {
+        get: () => props[key as any],
+        set: (value: V<Props>) => {
+          (this.props as Record<string, any>)[key] = value;
+        },
+      });
+    });
+  }
+
+  getElement(): E {
+    return this.element as E;
+  }
+
+  baseMethod() {
+    return 'baseMethod';
+  }
+}
+
+export class Sub extends WithBase<SubProps, HTMLDivElement>() {
+  constructor(props: Partial<SubProps>) {
+    super({
+      id: 'foo',
+      subA: 's',
+      subB: 4,
+      ...props,
+    });
+  }
+
+  subMethod() {
+    return 'subMethod';
+  }
+}
+
+export class Sub1 extends WithBase<SubProps1, HTMLLabelElement, Sub>(Sub) {
+  constructor(props: Partial<SubProps>) {
+    super({
+      id: 'foo',
+      subA: 's',
+      subB: 4,
+      subC: false,
+      ...props,
+    });
+  }
+
+  sub1Method() {
+    return 'sub1Method';
+  }
+}
+
+const sub = new Sub({ id: 'a' });
+sub.getElement();
+sub.baseMethod();
+sub.subMethod();
+const subEl = sub.element;
+const subId = sub.id;
+const subA = sub.subA;
+const subB = sub.subB;
+
+const sub1 = new Sub1({ id: 'b' });
+sub1.getElement();
+sub1.baseMethod();
+sub1.subMethod();
+sub1.sub1Method();
+const sub1El = sub1.element;
+const sub1Id = sub1.id;
+const sub1SubA = sub1.subA;
+const sub1SubB = sub1.subB;
+const sub1SubC = sub1.subC;
+
+(window as any).sub = sub;
+(window as any).sub1 = sub1;
