7c437f87b4236489eea0c31ccf3857fd639e9e6f
Ali Chamas <ali.chamas@play.co>
2022-02-08 10:20:23 +1100

    format

--- src/core/fileFormat.ts
+++ src/core/fileFormat.ts
@@ -268,12 +268,15 @@ class FileFormatReader {
 /** Test... */
 
 const writer = new FileFormatWriter();
+writer.writeFloat32(10000.102);
+writer.writeFloat32(101.103);
 writer.writeString('hello');
 writer.writeUint16(1000);
 writer.writeUint16(2000);
-// writer.writeUint8(255);
-// writer.writeUint8(126);
-writer.writeFloat32(10000.102);
+writer.writeUint8(255);
+writer.writeUint8(126);
+writer.writeUint16(1);
+writer.writeString('hello2');
 
 const onMouseDown = e => {
   writer.toDownloadLink('file.dat');
@@ -285,11 +288,14 @@ writer.toBase64().then(base64 => {
   console.log('base64', base64);
   const reader = new FileFormatReader();
   reader.fromBase64(base64).then(() => {
+    console.log(reader.readFloat32().toFixed(3));
+    console.log(reader.readFloat32().toFixed(3));
     console.log(reader.readString());
     console.log(reader.readUint16());
     console.log(reader.readUint16());
-    // console.log(reader.readUint8());
-    // console.log(reader.readUint8());
-    console.log(reader.readFloat32().toFixed(3));
+    console.log(reader.readUint8());
+    console.log(reader.readUint8());
+    console.log(reader.readUint16());
+    console.log(reader.readString());
   });
 });
